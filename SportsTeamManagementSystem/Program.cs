baff9355eef8e40dbd1c3a628aabfc58aba8cab1
ignacybanet <ignacybanet@cyfroweszkoly.pl>
2024-12-23 19:19:45 +0100

    add UpdateScore method

--- SportsTeamManagementSystem/Program.cs
+++ SportsTeamManagementSystem/Program.cs
@@ -1,9 +1,30 @@
 ﻿namespace SportsTeamManagementSystem;
 
+public class Player
+{
+    public string Name;
+    public string Position;
+    public int Score;
+    
+    public string UpdateScore(string playerName, int newScore)
+    {
+        if (this.Name == playerName)
+        {
+            this.Score = newScore;
+        }
+        return this.Score.ToString();
+    }
+}
+
+
 internal class Program
 {
     public static void Main(string[] args)
     {
-        
+        Player player1 = new Player();
+        player1.Name = "Alex";
+        Console.WriteLine(player1.Name);
+        player1.UpdateScore("Alex", 15);
+        Console.WriteLine(player1.Score.ToString());
     }
 }
